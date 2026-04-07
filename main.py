79ae7c9c83b24f3445ba03ca9d3bf08c2d374245
3nerg0n <130059729+3nerg0n@users.noreply.github.com>
2026-04-07 16:00:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ ALLOWED_IP_PREFIXES = [
 ]
 
 # Ключевые слова для фильтрации по названию
-TARGET_KEYWORDS = ["🇩🇪", "germany", "🇳🇱", "netherlands", "🇫🇮", "finland", "RU", "russia"]
+TARGET_KEYWORDS = ["🇩🇪", "germany", "🇳🇱", "netherlands", "🇫🇮", "finland"]
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
