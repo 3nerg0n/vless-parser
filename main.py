ae749a6c474ca340fe48836f3355da337e81c680
3nerg0n <130059729+3nerg0n@users.noreply.github.com>
2026-04-15 09:55:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 # --- КОНФИГУРАЦИЯ ---
 SOURCE_URLS = [
-    "https://etoneya.a9fm.site/1",
+    "https://gitverse.ru/api/repos/nloverx/EtoNeYa_Subs/raw/branch/master/1",
     "https://gitverse.ru/api/repos/bywarm/rser/raw/branch/master/wl.txt",
     "https://gitverse.ru/api/repos/bywarm/rser/raw/branch/master/selected.txt",
     "https://gitverse.ru/api/repos/bywarm/rser/raw/branch/master/merged.txt",
