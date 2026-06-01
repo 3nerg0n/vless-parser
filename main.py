070e91f29d85fb47978fefbe52bf2b2c192edade
3nerg0n <130059729+3nerg0n@users.noreply.github.com>
2026-06-01 10:19:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -49,9 +49,9 @@ MAX_WORKERS = 40 # Можно еще увеличить, так как пров�
 TARGET_KEYWORDS = ["🇩🇪", "germany", "🇳🇱", "netherlands", "🇫🇮", "finland", "🇸🇪", "sweden"]
 
 # 2. Фильтр по IP (только для тех, кто прошел фильтр по стране)
-ALLOWED_IP_PREFIXES = [
-    "51.250", "158.160"
-]
+#ALLOWED_IP_PREFIXES = [
+#    "51.250", "158.160"
+#]
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
